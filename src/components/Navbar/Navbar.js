39e9226464bf107faf308a964311e1f60c6d45da
himanxu1 <technophilic000@gmail.com>
2022-11-06 12:20:57 +0530

    responsive-navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './Navbar.css';
+import {GiHamburgerMenu} from 'react-icons/gi'
 
 const Navbar = () => {
+  const [showlinks,setShowLinks] = useState(true);
   return (
     <div className='navbar_container'>
         <div className='logo'>
             <h1><span>Meta</span><span>Scale</span></h1>
         </div>
-        <ul>
+        <ul  id={showlinks ? "hidden":""}>
             <li>Services</li>
             <li>Contact US</li>
         </ul>
+       <button style={{background:"transparent",border:"none"}} onClick={()=>setShowLinks(!showlinks)}><GiHamburgerMenu style={{fontSize:"30px",color:"white",marginRight:"20px"}}/></button>
     </div>
   )
 }
