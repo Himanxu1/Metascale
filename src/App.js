47018a0c1697b425351eb075ce845df693f66625
himanxu1 <technophilic000@gmail.com>
2022-11-06 17:55:01 +0530

    added cta and animation

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import './App.css';
+import Cta from './components/cta/Cta';
 import Footer from './components/Footer/Footer';
 import Hero from './components/Hero/Hero';
 import Navbar from './components/Navbar/Navbar';
 import Services from './components/services/Services';
 import Testimonial from './components/testimonial/Testimonial';
+import Fade from 'react-reveal/Fade';
 
 function App() {
   return (
     <div className="App">
+      <Fade bottom>
       <Navbar/>
       <Hero/>
       <Services/>
       <Testimonial/>
+      <Cta/>
       <Footer/>
+      </Fade>
     </div>
   );
 }
